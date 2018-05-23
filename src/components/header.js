4aa9bfaef1df67f7f56f18650455ce2b359d3d94
Myrzaliyev Turganbay <t.myrzaliyev@mail.ru>
2018-05-23 13:45:10 +0600

    first init

---
+++ src/components/header.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+
+class Header extends Component {
+    
+    inputChangeHandler(event) {
+        console.log(event.target.value)
+    }
+    
+    render() {
+        return (
+            <header>
+                <div className="logo">Logo</div>
+                <input type="text" onChange={this.inputChangeHandler} />
+            </header>    
+        )
+    }
+
+}
+
+export default Header
